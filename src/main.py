9620becb40c3e672784524f3eca93a77878c40b6
Nathan Paul <mail@polarhive.net>
2023-11-05 21:49:52 +0530

    feat/gui: display text, capture keys print keypresses to stdout

--- src/main.py
+++ src/main.py
@@ -5,9 +5,32 @@
 # author: PES2UG23CSXXX 
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+# import stuff
 import tkinter as tk
 
+# setup
 root = tk.Tk()
 root.title('Typing Speed Test')
 root.geometry('900x600')
+
+# functions
+def sendKeys(event):
+    try:
+        print(event.char)
+    except tk.TclError:
+        pass
+
+# paragraph text
+para = [
+    "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+]
+
+# display the paragraph
+text_widget = tk.Text(root, wrap=tk.WORD, height=5, width=40)
+text_widget.pack()
+text_widget.insert('1.0', para[0])
+
+# capture the keys
+root.bind('<KeyPress>', sendKeys)
 root.mainloop()
+
