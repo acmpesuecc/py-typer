5f6f7c43160ef55524dabb4135cf378ee8654d2b
NilayS08 <145572273+NilayS08@users.noreply.github.com>
2023-11-18 09:53:54 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 # author: PES2UG23CS368 Nathan Matthew Paul
 # author: PES2UG23CS3XX Navneet Nayak
 # author: PES2UG23CS3XX Nevin Mathew Thomas
-# author: PES2UG23CS3XX Nilay Srivastava
+# author: PES2UG23CS390 Nilay Srivastava
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright (C) 2023
 
