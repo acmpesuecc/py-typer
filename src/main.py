0b44e165adf2880a5582b68cd58e99d163335c88
NavneetNayak <54245165+NavneetNayak@users.noreply.github.com>
2023-12-09 11:05:23 +0530

    Update timer
    Fixed timer going too fast issue, removed border function as it was useless, put wpm, accuracy and time widgets on grid

--- src/main.py
+++ src/main.py
@@ -5,10 +5,10 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright (C) 2023
 
-import random
 import threading
 import simpleaudio as sa
 from tkinter import *
+from tkinter import ttk
 
 def play_sound_async():
     wave_obj = sa.WaveObject.from_wave_file("key.wav")
@@ -19,25 +19,29 @@ class Window:
 
     def __init__(self):
         self.window = Tk()
-        self.window.title('py-typer')
         self.window.configure(background="gray25")
         self.window.geometry("720x480")
         self.window.resizable(False, False)
+        self.frame = ttk.Frame(self.window, padding=10)
+        self.frame.grid()
+        self.window.grid_columnconfigure((0,1), weight=1)
+        self.window.grid_rowconfigure((0,1, 2, 3, 4), weight=1)
         self.restarted = False
         self.setup()
 
     def setup(self):
-        self.spelled = 1
-        self.accuracy = 0
+
         self.misspelled = 0
+        self.spelled = 1
+        self.total_time = 41
+        self.accuracy = 0
         self.wpm = 0
-        self.time_mode = 40
-        self.total_time = self.time_mode + 1
-        self.type_time = self.total_time - 1
         self.write_able = True
         self.cursor_blink = True
 
-        text = "python, cython, jython, pypy, pithon" 
+        self.type_time = self.total_time - 1
+
+        text = "python is python is python is python is python is python is python is" 
         self.title_label = Label(self.window, text="py-typer", font=("roboto condensed", 66), fg="#ebc934", background="gray25")
         self.title_label.place(rely=0.05, relx=0.01, anchor=W)
 
@@ -48,20 +52,19 @@ class Window:
         self.typed_text.place(relx=0.5, rely=0.5, anchor=E)
 
         self.time_label = Label(self.window, text=self.type_time, font=("roboto condensed", 30), fg="#ebc934", background="gray25")
-        self.time_label.place(relx=0.95, rely=0.4, anchor=CENTER)
+        self.time_label.grid(row=1, column=2, sticky=S)
 
         self.accuracy_label = Label(self.window, text=self.accuracy, font=("roboto condensed", 30), fg="#ebc934", background="gray25")
-        self.accuracy_label.place(relx=0.85, rely=0.4, anchor=CENTER)
+        self.accuracy_label.grid(row=1, column=3, sticky=S)
 
         self.wpm_label = Label(self.window, text=self.wpm, font=("roboto condensed", 30), fg="#ebc934", background="gray25")
-        self.wpm_label.place(relx=0.7, rely=0.4, anchor=CENTER)
+        self.wpm_label.grid(row=1, column=4, sticky=S)
 
         self.cursor_label = Label(self.window, text="||", background="gray25", fg="gray60", font=("roboto", 20), wraplength=1)
         self.cursor_label.place(relx=0.499, rely=0.51, anchor=CENTER)
 
         self.calculate_accuracy()
         self.calculate_wpm()
-        self.border()
         self.cursor_blinking()
         
         self.window.bind('<KeyPress>', self.key_press)
@@ -82,11 +85,11 @@ class Window:
         restart_button = Button(self.window, text="Restart",font=("roboto", 30), background="gray25", command=self.restart, highlightbackground="gray25", fg="#ebc934")
         restart_button.place(rely=0.6, relx=0.5, anchor=CENTER)
 
-        mode_button = Button(self.window, text="Mode", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25")
+        mode_button = Button(self.window, text="Mode", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25", bg="blue")
         mode_button.place(rely=0.8, relx=0.5, anchor=CENTER)
 
     def key_press(self,event):
-        if not self.restarted:
+        if not self.restarted :
             if self.spelled == 1:
                 self.countdown()
         if not self.write_able:
@@ -106,7 +109,8 @@ class Window:
     def countdown(self):
         if self.type_time > 0:
             self.type_time -= 1
-            try: self.time_label.configure(text=self.type_time)
+            try:
+                self.time_label.configure(text=self.type_time)
             except TclError: pass
             self.window.after(1000, self.countdown)
         else:
@@ -120,9 +124,9 @@ class Window:
         except TclError: pass
 
     def calculate_wpm(self):
-        words_typed = len(self.typed_text.cget('text').split())
-        elapsed_time = self.total_time - self.type_time
         try:
+            words_typed = len(self.typed_text.cget('text').split())
+            elapsed_time = self.total_time - self.type_time
             self.wpm = str(int((words_typed / elapsed_time) * 60)) + " WPM"
             self.wpm_label.configure(text=self.wpm)
         except TclError:
@@ -135,13 +139,6 @@ class Window:
     def modes(self):
         pass
 
-    def border(self):
-        edge_border_left = Label(self.window, wraplength=1, text="h"*420, fg="gray25", background="gray25")
-        edge_border_left.place(rely=0.1)
-        edge_border_right = Label(self.window, wraplength=1, text="__"*420, fg="gray25", background="gray25")
-        edge_border_right.place(rely=0.1, relx=0.99)
-        self.window.after(500, self.border)
-
     def cursor_blinking(self):
         if self.cursor_blink:
             try: self.cursor_label.configure(text="")
@@ -152,6 +149,6 @@ class Window:
             except TclError: pass
             self.cursor_blink = True
         self.window.after(500, self.cursor_blinking)
-
+        
 window = Window()
 window.window.mainloop()
