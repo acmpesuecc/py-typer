3822508cb2e40cd5d98b15c37e4b6cf7761f19ee
NavneetNayak <54245165+NavneetNayak@users.noreply.github.com>
2023-12-18 01:21:44 +0530

    proper graph, finally fixed time issue, shift key
    good graph, fixed time going too fast issue, made it so shift key can be pressed without accuracy decreasing

--- src/main.py
+++ src/main.py
@@ -13,11 +13,15 @@ import matplotlib.pyplot as plt
 import text_module
 from tkinter import *
 from tkinter import ttk
+from matplotlib.figure import Figure 
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,  NavigationToolbar2Tk) 
 
 def play_sound_async():
-    wave_obj = sa.WaveObject.from_wave_file("key.wav") # make sure you have this file locally
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
+    try:
+        wave_obj = sa.WaveObject.from_wave_file("key.wav") # make sure you have this file locally
+        play_obj = wave_obj.play()
+        play_obj.wait_done()
+    except: pass
 
 class Window:
     def td(self, s):
@@ -27,44 +31,53 @@ class Window:
 
     def choose_td(self):
         self.clear()
-        Button(self.window, text="30s", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25", command=lambda: self.td(30)).place(rely=0.2, relx=0.5, anchor=CENTER)
+        Button(self.window, text="30s", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25", command=lambda: self.td(30)).place(rely=0.5, relx=0.2, anchor=CENTER)
         Button(self.window, text="60s", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25", command=lambda: self.td(60)).place(rely=0.5, relx=0.5, anchor=CENTER)
-        Button(self.window, text="120s", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25", command=lambda: self.td(120)).place(rely=0.8, relx=0.5, anchor=CENTER)
+        Button(self.window, text="120s", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25", command=lambda: self.td(120)).place(rely=0.5, relx=0.8, anchor=CENTER)
 
     def wd(self, m):
         self.clear()
+        self.stop_threads = True
         self.word_difficulty = m
         self.restart()
 
     def choose_wd(self):
         self.clear()
-        Button(self.window, text="easy", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25", command=lambda: self.wd(1)).place(rely=0.1, relx=0.5, anchor=CENTER)
-        Button(self.window, text="mid", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25", command=lambda: self.wd(2)).place(rely=0.4, relx=0.5, anchor=CENTER)
-        Button(self.window, text="hard", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25", command=lambda: self.wd(3)).place(rely=0.7, relx=0.5, anchor=CENTER)
-        Button(self.window, text="freestyle", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25", command=lambda: self.wd(0)).place(rely=0.9, relx=0.5, anchor=CENTER)
+        Button(self.window, text="easy", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25", command=lambda: self.wd(1)).place(rely=0.4, relx=0.2, anchor=CENTER)
+        Button(self.window, text="medium", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25", command=lambda: self.wd(2)).place(rely=0.4, relx=0.5, anchor=CENTER)
+        Button(self.window, text="hard", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25", command=lambda: self.wd(3)).place(rely=0.4, relx=0.8, anchor=CENTER)
+        Button(self.window, text="freestyle", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25", command=lambda: self.wd(0)).place(rely=0.6, relx=0.5, anchor=CENTER)
 
     def __init__(self):
+        self.stop_threads = False
+
         self.window = Tk()
         self.window.title('py-typer')
         self.window.configure(background="gray25")
-        self.window.geometry("720x480")
-        # self.window.resizable(False, False)
-        self.window.grid_columnconfigure((0, 1), weight=1)
-        self.window.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)
+        self.window.geometry("720x500")
+        self.window.resizable(False, False)
         self.frame = ttk.Frame(self.window, padding=10).grid()
 
+        self.restarted = False
+
+        self.window.grid_columnconfigure((0, 1), weight=1)
+        self.window.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)
+
+        self.graph = True
         self.time_difficulty = 30 # 30s for now to easily debug WPM and accuracy
         self.word_difficulty = 1  # 1: means easy, 2: means hard, 3: means difficult, 0: means freestyle mode
-        self.x=[]
-        self.y=[]
 
         self.setup()
 
     def clear(self):
         for widget in self.window.winfo_children():
             widget.destroy()
-
+        
     def setup(self):
+
+        self.x=[]
+        self.y=[]
+
         self.misspelled = 0
         self.spelled = 1
         self.total_time = self.time_difficulty + 1
@@ -108,49 +121,66 @@ class Window:
         self.window.bind('<KeyPress>', self.key_press)
 
     def restart(self):
+        self.restarted = True
+        self.stop_threads = False
         self.clear()
         self.setup()
 
     def plot_graph(self):
-        plt.clf()
-        plt.plot(self.x, self.y)
-        plt.xlabel('Time')
-        plt.ylabel('WPM')
-        plt.title('Typing Performance')
-        plt.show()
-        self.x=[]
-        self.y=[]
+        self.fig = Figure(figsize = (3, 2), dpi = 100) 
+        self.fig.clf()
+        plot1 = self.fig.add_subplot() 
+        plot1.plot(self.x, self.y, color="#ebc934")
+        plot1.set_facecolor("#404040")
+        plot1.tick_params(axis='x', colors="grey")
+        plot1.tick_params(axis='y', colors="grey")
+        plot1.spines['left'].set_color('#404040')        # setting up Y-axis tick color to red
+        plot1.spines['top'].set_color('#404040')
+        plot1.spines['right'].set_color('#404040')        # setting up Y-axis tick color to red
+        plot1.spines['bottom'].set_color('#404040')
+        self.fig.patch.set_facecolor("#404040")
+        canvas = FigureCanvasTkAgg(self.fig, master = self.window)   
+        canvas.draw()
+        canvas.get_tk_widget().grid(row=1, column=2)
 
     def main_menu(self):
         self.clear()
-        results_label = Label(self.window, text=self.wpm + "  " + self.accuracy, font=("roboto", 80, "bold"), background="gray25", fg="#ebc934")
-        results_label.place(relx=0.5, rely=0.4, anchor=CENTER)
+
+        results_label = Label(self.window, text=self.wpm + "  " + self.accuracy, font=("roboto", 50, "bold"), background="gray25", fg="#ebc934")
+        results_label.grid(row=1, column=0)
 
         restart_button = Button(self.window, text="Restart", font=("roboto", 30), background="gray25", command=self.restart, highlightbackground="gray25", fg="#ebc934")
-        restart_button.place(rely=0.6, relx=0.5, anchor=CENTER)
+        restart_button.grid(row=2, column=0)
 
         mode_button = Button(self.window, text="Mode", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25", bg="blue", command=self.modes)
-        mode_button.place(rely=0.8, relx=0.5, anchor=CENTER)
+        mode_button.grid(row=2, column=2)
 
         self.plot_graph()
 
     def key_press(self, event):
-        if self.spelled == 1:
-            self.start_timer()
         if not self.write_able:
             return None
-        if event.char == self.untyped_text.cget('text')[:1]:
-            sound_thread = threading.Thread(target=play_sound_async)
-            sound_thread.start()
-            self.typed_text.configure(text=self.typed_text.cget('text') + event.char)
-            self.untyped_text.configure(text=self.untyped_text.cget('text')[1:])
-            self.spelled += 1
-            if len(self.untyped_text.cget('text')) < 1:
-                self.clear()
-                self.main_menu()
-        else:
-            self.misspelled += 1
-        self.calculate_accuracy()
+        if event.keysym == "Shift_L" or event.keysym == "Shift_R":
+            return None
+        if self.spelled == 1:
+            self.start_timer()
+        try:
+            if event.char == self.untyped_text.cget('text')[:1]:
+                try:
+                    sound_thread = threading.Thread(target=play_sound_async)
+                    sound_thread.start()
+                except: pass
+                self.typed_text.configure(text=self.typed_text.cget('text') + event.char)
+                self.untyped_text.configure(text=self.untyped_text.cget('text')[1:])
+                self.spelled += 1
+                if len(self.untyped_text.cget('text')) < 1:
+                    self.clear()
+                    self.stop_threads = True
+                    self.main_menu()
+            else:
+                self.misspelled += 1
+            self.calculate_accuracy()
+        except TclError: pass
 
     def start_timer(self):
         self.timer_thread = threading.Thread(target=self.countdown_thread)
@@ -159,6 +189,8 @@ class Window:
 
     def countdown_thread(self):
         while self.type_time > 0 and self.write_able:
+            if self.stop_threads:
+                break
             time.sleep(1)
             self.type_time -= 1
             try:
@@ -168,6 +200,7 @@ class Window:
 
         if self.write_able:
             self.write_able = False
+            self.stop_threads = True
             self.main_menu()
 
     def calculate_accuracy(self):
@@ -192,8 +225,8 @@ class Window:
  
     def modes(self):
         self.clear()
-        Button(self.window, text="Time Difficulty", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25", command=self.choose_td).place(rely=0.2, relx=0.5, anchor=CENTER)
-        Button(self.window, text="Word Difficulty", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25", command=self.choose_wd).place(rely=0.5, relx=0.5, anchor=CENTER)
+        Button(self.window, text="Time Difficulty", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25", command=self.choose_td).place(rely=0.4, relx=0.5, anchor=CENTER)
+        Button(self.window, text="Word Difficulty", font=("roboto", 30), highlightbackground="gray25", fg="#ebc934", background="gray25", command=self.choose_wd).place(rely=0.6, relx=0.5, anchor=CENTER)
 
     def cursor_blinking(self):
         if self.cursor_blink:
